1e6fa54c279ec6ed7f4b9686cb47d168ba873880
sela16-meet <sela.peleg@gmail.com>
2016-07-20 09:46:30 +0300

    Finally a Freaking Github

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+ 	return render_template("welcome.html")
+
+@app.route('/signin')
+def signin():
+ 	return render_template("signin.html")
+
+
+@app.route('/signup')
+def signup():
+ 	return render_template("signup.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
+
+@app.route('/home')
+def home():
+	return render_template("home.html")
+
+@app.route('/story')
+def story():
+	return render_template("story.html")
+
+if __name__=="__main__":
+ 	app.run()
